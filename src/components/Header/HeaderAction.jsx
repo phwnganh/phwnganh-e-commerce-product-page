a38487503e00678f697ece2b65cf7f42a67fe562
phwnganh <phuonganh25102003@gmail.com>
2026-04-12 10:07:21 +0700

    fix: spacing between items on header action

--- src/components/Header/HeaderAction.jsx
+++ src/components/Header/HeaderAction.jsx
@@ -2,13 +2,13 @@ import CartIcon from '../../assets/icon-cart.svg'
 import Avatar from '../../assets/image-avatar.png'
 const HeaderAction = () => {
     return (
-        <div className={"flex items-center gap-12"}>
-            <div className={"flex justify-center items-center shrink-0"}>
+        <div className={"flex items-center gap-6 sm:gap-12"}>
+            <button type={"button"} className={"flex justify-center items-center shrink-0"}>
                 <img src={CartIcon} alt={"cart-icon"}/>
-            </div>
-            <div className={"flex justify-center items-center w-12.5 h-12.5 shrink-0"}>
+            </button>
+            <button type={"button"} className={"flex justify-center items-center w-6 h-6 sm:w-12.5 sm:h-12.5 shrink-0"}>
                 <img src={Avatar} alt={"avatar"}/>
-            </div>
+            </button>
         </div>
     );
 };
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -3,7 +3,7 @@ import HeaderAction from "./HeaderAction.jsx";
 
 const Header = () => {
     return (
-        <header className={"flex flex-col gap-8"}>
+        <header className={"flex flex-col gap-8 px-6 sm:px-0"}>
             <div className={"flex items-center justify-between"}>
                 <Menu/>
                 <HeaderAction/>
