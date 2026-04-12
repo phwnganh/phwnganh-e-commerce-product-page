5dab4a676d2d84a92d928cf151ee55d215332456
phwnganh <phuonganh25102003@gmail.com>
2026-04-12 10:02:00 +0700

    fix: small bug uis

--- src/components/Main/ProductDescription/ProductDescriptionSection.jsx
+++ src/components/Main/ProductDescription/ProductDescriptionSection.jsx
@@ -4,7 +4,7 @@ import Button from "../../uis/Button.jsx";
 import CartIcon from '../../../assets/icon-cart.svg'
 const ProductDescriptionSection = () => {
     return (
-        <div className={"flex flex-col gap-4 sm:gap-6 md:gap-8"}>
+        <div className={"flex flex-col gap-4 sm:gap-6 md:gap-8 px-6 sm:px-0"}>
             <p className={"text-grey-500 text-preset-5 leading-preset-5 font-preset-5 uppercase"}>Sneaker Company</p>
             <h1 className={"text-preset-2 sm:text-preset-1 leading-preset-2 sm:leading-preset-1 font-preset-1"}>Fall Limited Edition Sneakers</h1>
             <p className={"text-preset-4 sm:text-preset-3 leading-preset-4 sm:leading-preset-3 text-grey-500"}>These low-profile sneakers are your perfect casual wear companion. Featuring a durable rubber outer sole, they’ll withstand everything the weather can offer.</p>
--- src/components/Main/ProductDescription/ProductQuantityPanel.jsx
+++ src/components/Main/ProductDescription/ProductQuantityPanel.jsx
@@ -1,15 +1,28 @@
 import MinusIcon from '../../../assets/icon-minus.svg'
 import PlusIcon from '../../../assets/icon-plus.svg'
+import {useState} from "react";
 const ProductQuantityPanel = () => {
+    const [quantity, setQuantity] = useState(0)
+
+    const handleIncreaseQuantity = () => {
+        setQuantity(quantity + 1)
+    }
+
+    const handleDecreaseQuantity = () => {
+        if(quantity > 0){
+            setQuantity(quantity - 1)
+
+        }
+    }
     return (
         <div className={"bg-grey-50 rounded-10 px-4 sm:px-6 md:px-7 py-4 flex items-center justify-between gap-26"}>
-            <div className={"flex justify-center items-center shrink-0 w-3 h-3"}>
+            <button onClick={handleDecreaseQuantity} type={"button"} aria-label={"decrease quantity"} className={"flex justify-center items-center shrink-0 w-3 h-3"}>
                 <img src={MinusIcon} alt="Minus" />
-            </div>
-            <span className={"text-preset-3 leading-preset-3 font-preset-3 text-grey-950"}>3</span>
-            <div className={"flex justify-center items-center shrink-0 w-3 h-3"}>
+            </button>
+            <span aria-label={"quantity"} className={"text-preset-3 leading-preset-3 font-preset-3 text-grey-950 w-8"}>{quantity}</span>
+            <button onClick={handleIncreaseQuantity} type={"button"} aria-label={"increase quantity"} className={"flex justify-center items-center shrink-0 w-3 h-3"}>
                 <img src={PlusIcon} alt="Plus"/>
-            </div>
+            </button>
         </div>
     );
 };
